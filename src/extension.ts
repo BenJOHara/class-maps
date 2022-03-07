71f8ae820ac49744b91925c4c23c93b364eb137d
Ben O'Hara <benohara132@gmail.com>
2022-03-07 13:41:48 +0000

    Adding commands and webviews
    Adding the commands and webviews that are needed, not implemented yet

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "class-maps" is now active!');
+	console.log('class-maps is now active');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('class-maps.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Class Maps!');
-	});
+	const provider = new ClassViewProvider(context.extensionUri);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(ClassViewProvider.viewType, provider));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('class-maps.show-names', () => {
+			
+		}));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('class-maps.show-number', () => {
+			
+		}));
+	
+	context.subscriptions.push(
+		vscode.commands.registerCommand('class-maps.show-class-info', () =>{
+			
+		}));
 }
 
-// this method is called when your extension is deactivated
+class ClassViewProvider implements vscode.WebviewViewProvider{
+
+	public static readonly viewType = 'class-maps.map-view';
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+
+	public resolveWebviewView()
+	{
+
+	}
+}
 export function deactivate() {}
