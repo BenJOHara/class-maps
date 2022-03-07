1d4748d85ee7999b4100d9674712d0301e5630c6
Ben O'Hara <benohara132@gmail.com>
2022-03-07 14:06:42 +0000

    Added js and .css files

---
+++ media/main.js
@@ -0,0 +1,161 @@
+//@ts-check
+// This script will be run within the webview itself
+// It cannot access the main VS Code APIs directly.
+(function () {
+    const vscode = acquireVsCodeApi();
+
+    const oldState = vscode.getState() || { content: [] };
+
+    /** @type {Array<{ value: string }>} */
+    let content = oldState.content;
+
+    //updateColorList(colors);
+
+    document.querySelector('.show-names').addEventListener('click', () => {
+        showNames();
+    });
+
+    document.querySelector('.show-number').addEventListener('click', () => {
+        showNumber();
+    });
+
+    document.querySelector('.show-class-info').addEventListener('click', () => {
+        showClassInfo();
+    });
+
+    // Handle messages sent from the extension to the webview
+    window.addEventListener('message', event => {
+        const message = event.data; // The json data that the extension sent
+        switch (message.type) {
+            case 'showNames':
+                {
+                    updateClassList(message.content);
+                    break;
+                }
+            case 'showNumber':
+                {
+                    updateNumberOfClasses(message.content);
+                    break;
+                }
+            case 'showClassInfo':
+                {
+                    updateClassInfo(message.content);
+                }
+
+        }
+    });
+
+    function showClassInfo() {
+        //get names
+        //add to ul class list
+        vscode.postMessage({type: 'getClassInfo'});
+    }
+
+    function showNames() {
+        //get names
+        //add to ul class list
+        vscode.postMessage({type: 'getNames'});
+    }
+
+    function showNumber(){
+        //get number of classes
+        //add to ul class list as one item
+        vscode.postMessage({type : 'getNumber'});
+    }
+
+
+    /**
+     * @param {Array<{ value: string }>} content
+     */
+    function updateClassList(content)
+    {
+        clearClassInfo();
+        clearNumberOfClasses();
+        const ul = document.querySelector('.class-list');
+        ul.textContent = '';
+        for (const cont of content){
+            const li = document.createElement('li');
+            li.className = 'class-entry';
+
+            const text = document.createElement('p');
+            text.innerText = cont;
+            li.appendChild(text);
+            ul.appendChild(li);
+        }
+        vscode.setState({ content: content });
+    }
+    
+    function clearClassInfo()
+    {
+        const ul = document.querySelector('.svg1');
+        ul.textContent = '';
+    }
+
+    function updateClassInfo(classInfo)
+    {
+        clearNumberOfClasses();
+        clearClassList();
+        const JSONtext = JSON.parse(classInfo);
+        const ul = document.querySelector('.svg1');
+        ul.textContent = '';
+        ul.setAttributeNS(null, 'width', '500');
+        ul.setAttributeNS(null, 'height', '1000');
+        for (const c of JSONtext){
+            console.log(c);
+            console.log(c.name);
+            console.log(c.length);
+            const g = document.createElement('g');
+            const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            const width = 0.25 * c.length;
+            const height = 0.5 * c.length;
+            rect.setAttributeNS(null, 'width', width.toString());
+            rect.setAttributeNS(null, 'height', height.toString());
+            rect.setAttributeNS(null, 'fill', 'white');
+            const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            text.textContent = classInfo.name;
+            g.appendChild(rect);
+            g.appendChild(text);
+            ul.appendChild(rect);
+            //ul.appendChild(g);
+        }
+        vscode.setState({ content: content });
+
+    }
+
+    function clearClassList()
+    {
+        const ul = document.querySelector('.class-list');
+        ul.textContent = '';
+    }
+
+    function clearNumberOfClasses()
+    {
+        const ul = document.querySelector('.svg1');
+        ul.textContent = '';
+    }
+
+    /**
+     * 
+     * @param {number} numberClasses 
+     */
+    function updateNumberOfClasses(numberClasses)
+    {
+        clearClassInfo();
+        clearClassList();
+        const ul = document.querySelector('.svg1');
+        ul.textContent = '';
+        ul.setAttributeNS(null, 'width', '500');
+        ul.setAttributeNS(null, 'height', '3000');
+        let height = 100 * numberClasses;
+        //<rect width="300" height="100" style="fill:rgb(0,0,255;);stroke-width:3;stroke:rgb(0,0,0)"/>;
+        const li = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        li.setAttributeNS(null, 'width', '100');
+
+        li.setAttributeNS(null, 'height', height.toString());
+        ul.appendChild(li);
+        vscode.setState({content: content});
+    }
+
+}());
+
+
